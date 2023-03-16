eabbffa737095f66f0232efa1ae8c9dfb60bf22b
Lenart Kos <koslenart@gmail.com>
2023-03-16 09:52:19 +0100

    Seman: Add typecheck template

--- prev23/src/prev23/Compiler.java
+++ prev23/src/prev23/Compiler.java
@@ -130,7 +130,9 @@ public class Compiler {
 
 				// Semantic analysis.
 				try (SemAn seman = new SemAn()) {
-					Abstr.tree.accept(new NameResolver(seman), null);
+					Abstr.tree.accept(new NameResolver(), null);
+					Abstr.tree.accept(new TypeResolver(), null);
+					Abstr.tree.accept(new AddrResolver(), null);
 					AbsLogger logger = new AbsLogger(seman.logger);
 					logger.addSubvisitor(new SemLogger(seman.logger));
 					Abstr.tree.accept(logger, null);
