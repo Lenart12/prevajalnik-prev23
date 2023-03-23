6fdba935eb597c4fa15f8d1a74de0e19c20f099a
Lenart Kos <koslenart@gmail.com>
2023-03-23 16:36:23 +0100

    Memory: Implement memory evaluator

--- prev23/src/prev23/phase/memory/MemEvaluator.java
+++ prev23/src/prev23/phase/memory/MemEvaluator.java
@@ -1,5 +1,6 @@
 package prev23.phase.memory;
 
+import prev23.common.report.Report;
 import prev23.data.ast.tree.*;
 import prev23.data.ast.tree.decl.*;
 import prev23.data.ast.tree.expr.*;
@@ -9,9 +10,163 @@ import prev23.data.typ.*;
 import prev23.data.mem.*;
 import prev23.phase.seman.*;
 
+import java.util.HashMap;
+
 /**
  * Computing memory layout: frames and accesses.
  */
-public class MemEvaluator extends AstFullVisitor<Object, Object> {
+public class MemEvaluator extends AstFullVisitor<Object, MemEvaluator.MemScope> {
+
+
+    public static class MemScope {
+        public int depth = 0;
+        public long locals_size = 0;
+        public long args_size = 0;
+        public long params_size = -8;
+        public HashMap<String, AstNameDecl> exported_names = new HashMap<>();
+
+        private MemScope() {}
+
+        public MemScope sub_scope() {
+            var sub_scope = new MemScope();
+            sub_scope.depth = depth + 1;
+            sub_scope.exported_names = exported_names;
+            return sub_scope;
+        }
+
+        public MemScope record_scope() {
+            if (depth == -1) {
+                return this;
+            }
+
+            var record_scope = new MemScope();
+            record_scope.depth = -1;
+
+            return record_scope;
+        }
+
+        private MemLabel get_label(AstNameDecl decl) {
+            if (depth <= 1 && !exported_names.containsKey(decl.name)) {
+                exported_names.put(decl.name, decl);
+                return new MemLabel(decl.name);
+            } else {
+                return new MemLabel();
+            }
+        }
+
+        public void add_frame(AstFunDecl decl) {
+            var frame = new MemFrame(get_label(decl), depth, locals_size, args_size);
+            Memory.frames.put(decl, frame);
+        }
+
+        public void reserve_args(AstCallExpr call) {
+            var call_args_size = 8;
+            var fun_decl = (AstFunDecl)SemAn.declaredAt.get(call);
+
+            for (var par: fun_decl.pars) {
+                call_args_size += SemAn.isType.get(par.type).size();
+            }
+
+            args_size = Math.max(args_size, call_args_size);
+        }
+        
+        public void add_access(AstMemDecl decl) {
+            MemAccess access = null;
+
+            var type = SemAn.isType.get(decl.type);
+            if (type == null) throw new Report.InternalError();
+            var mem_size = type.size();
+
+            if (depth == 0) {
+                access = new MemAbsAccess(mem_size, get_label(decl));
+            } else {
+                var access_depth = Math.max(0, depth);
+
+                if (decl instanceof AstParDecl) {
+                    params_size -= mem_size;
+                    access = new MemRelAccess(mem_size, params_size, access_depth);
+                } else {
+                    access = new MemRelAccess(mem_size, locals_size, access_depth);
+
+                    if (!(type instanceof SemRec))
+                        locals_size += mem_size;
+                }
+            }
+
+            Memory.accesses.put(decl, access);
+        }
+    }
+
+    // DECLARATIONS
+
+    @Override
+    public Object visit(AstTrees<?> trees, MemScope scope) {
+        if (scope == null) scope = new MemScope();
+        return super.visit(trees, scope);
+    }
+
+    @Override
+    public Object visit(AstVarDecl varDecl, MemScope scope) {
+        scope.add_access(varDecl);
+        varDecl.type.accept(this, scope);
+        return null;
+    }
+
+    @Override
+    public Object visit(AstRecType recType, MemScope scope) {
+        var record_scope = scope.record_scope();
+        for (var comp: recType.comps) {
+            comp.accept(this, record_scope);
+        }
+        return null;
+    }
+    @Override
+    public Object visit(AstCmpDecl cmpDecl, MemScope scope) {
+        scope.add_access(cmpDecl);
+        cmpDecl.type.accept(this, scope);
+        return null;
+    }
+
+    @Override
+    public Object visit(AstFunDecl funDecl, MemScope scope) {
+        var function_scope = scope.sub_scope();
+
+        for (var pars: funDecl.pars) {
+            pars.accept(this, function_scope);
+        }
+
+        if (funDecl.stmt != null) {
+            funDecl.stmt.accept(this, function_scope);
+        }
+
+        function_scope.add_frame(funDecl);
+        return null;
+    }
+
+    @Override
+    public Object visit(AstParDecl parDecl, MemScope scope) {
+        scope.add_access(parDecl);
+        return null;
+    }
+
+    // EXPRESSIONS
+
+    @Override
+    public Object visit(AstCallExpr callExpr, MemScope scope) {
+        scope.reserve_args(callExpr);
+        return null;
+    }
+
+    @Override
+    public Object visit(AstAtomExpr atomExpr, MemScope scope) {
+        if (atomExpr.type != AstAtomExpr.Type.STR)
+            return null;
+
+        var value = atomExpr.value.substring(1, atomExpr.value.length() - 1).replace("\\\"", "\"");
+        var const_size = (value.length() + 1) * (new SemChar().size());
+
+        Memory.strings.put(atomExpr, new MemAbsAccess(const_size, new MemLabel(), value));
+        return null;
+    }
 
 }
